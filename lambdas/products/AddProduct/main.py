3ea0edb7e9a7ebd9e41ccb9dfe13f7ed7aef404e
ardome21 <ardome21@gmail.com>
2025-12-12 14:30:10 -0500

    clix

--- lambdas/products/AddProduct/main.py
+++ lambdas/products/AddProduct/main.py
@@ -4,19 +4,23 @@ import json
 from auth_utils import require_role
 
 @require_role('admin')
-def lambda_hander(event, _context):
+def lambda_handler(event, _context):
     """
     Entry point to lambda function
     """
     try:
         print("Begin Script...")
-        print(f"Event: {event}")
+        print(f"Event body: {event['body']}")
+        return {
+            'statusCode': 200, 
+            'body': json.dumps({'message': 'Successfully exited script'})
+        }
     except Exception as e:
-        print(f"Error verifying email: {e}")
+        print(f"Error Adding Product: {e}")
         return {
             'statusCode': 500,
             'body': json.dumps({
                 'message': 'F',
-                'error': 'Internal server error'
+                'error': f'Internal server error: {e}'
                 })
         }
